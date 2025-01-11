e5ce789dbce9a4079c4a64c54098ecf84e4c4d74
Feras Jobeir <fjobeir@gmail.com>
2025-01-11 20:25:37 +0300

    try without seo

--- src/app/[locale]/races/[slug]/page.tsx
+++ src/app/[locale]/races/[slug]/page.tsx
@@ -1,23 +1,23 @@
 import SingleRace from "@/components/race/single";
 import { getRace } from "@/lib/api/wp";
-import { seoContent } from "@/lib/seo/seo";
+// import { seoContent } from "@/lib/seo/seo";
 import { Locale, MVRace } from "@/lib/types/misc";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type Props = {
   params: Promise<{ locale: Locale; slug: string }>;
 };
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const locale = (await params).locale;
-  const slug = (await params).slug;
-  const race = await getRace(slug, locale);
-  return await seoContent({
-    page: "race",
-    locale,
-    data: race,
-  });
-}
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   const locale = (await params).locale;
+//   const slug = (await params).slug;
+//   const race = await getRace(slug, locale);
+//   return await seoContent({
+//     page: "race",
+//     locale,
+//     data: race,
+//   });
+// }
 
 export default async function SingleRacePage({ params }: Props) {
   const locale = (await params).locale;
